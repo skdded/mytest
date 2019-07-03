2d1ac19cdf25308394f89578ba2ad4dd775355ef
skdded <bos@itcast.cn>
2019-07-04 01:30:04 +0800

    提交java代码

---
+++ git_day01_demo/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,17 @@
+package com.itheima.domain;
+
+/**
+ * @ClassName User
+ * @Description TODO
+ * @Author ly
+ * @Company 深圳黑马程序员
+ * @Date 2019/1/20 11:56
+ * @Version V1.0
+ */
+public class User {
+    private Integer id;
+    private String name;
+    private Integer age;
+    // 前端人员开发
+    private String qianduan;
+}
